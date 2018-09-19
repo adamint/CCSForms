cca0b1b13b433a214fbb8382634af4a8378973fa
Adam Ratzman <adam@adamratzman.com>
2018-09-19 09:15:51 -0400

    added error and 404 pages

--- FormFrontend/src/main/kotlin/com/adamratzman/forms/frontend/FormFrontend.kt
+++ FormFrontend/src/main/kotlin/com/adamratzman/forms/frontend/FormFrontend.kt
@@ -28,7 +28,22 @@ class FormFrontend {
 
         registerHandlebarsHelpers()
 
+        notFound { request, response ->
+            val map = getMap(request, "Not Found")
+            map["notFound"] = true
+            map["description"] = "The page you were looking for couldn't be found :("
+            handlebars.render(ModelAndView(map, "error.hbs"))
+        }
+
+        internalServerError { request, response ->
+            val map = getMap(request, "Something went wrong!")
+            map["title"] = "Server Error"
+            map["description"] = "Please try again soon"
+            handlebars.render(ModelAndView(map, "error.hbs"))
+        }
+
         exception(Exception::class.java) { exception, _, _ -> exception.printStackTrace() }
+
 
         get("/") { request, _ ->
             val map = getMap(request, "Home")
